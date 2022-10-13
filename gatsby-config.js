b41a71047ae86d89a87109da96118f2a656efe65
Serega1288 <svtomusjak@gmail.com>
2022-10-13 13:20:40 +0300

    new up

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,14 +85,14 @@ module.exports = {
     //     //path: path.join(__dirname, `src`, `images`),
     //   },
     // },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: "images",
-        path: "./src/images/",
-        //path: path.join(__dirname, `src`, `images`),
-      },
-    },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     name: "images",
+    //     path: "./src/images/",
+    //     //path: path.join(__dirname, `src`, `images`),
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-offline`,
       options: {
