b9bb27bee83948387a1eecf09c766cc03de5272a
Serega1288 <svtomusjak@gmail.com>
2022-11-02 13:42:59 +0200

    data new ...

--- src/components/constructor/Const_Item.js
+++ src/components/constructor/Const_Item.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Banner from './Banner';
-import Collections from './Collections';
-import Video from './Video';
-import Instagram from './Instagram';
+// import Banner from './Banner';
+// import Collections from './Collections';
+// import Video from './Video';
+// import Instagram from './Instagram';
 import Slider from './Slider';
 import Title from './Title';
 import Content1 from './Content_text_img_img';
@@ -10,8 +10,8 @@ import Content2 from './Content_img_title_text';
 import Collaps from './Collaps';
 import Content3 from './ContentCols';
 import Editor from "./Editor";
-import Contactdata from "./Contactdata";
-import Contactform from "./Contactform";
+// import Contactdata from "./Contactdata";
+// import Contactform from "./Contactform";
 
 
 const Const = ( { item } ) => {
@@ -21,10 +21,10 @@ const Const = ( { item } ) => {
             { item.fieldGroupName ? (
                 <>
                     { item.fieldGroupName == 'Page_Acfconstructor_Const_Editor' ? ( <Editor item={item} /> ) : null }
-                    { item.fieldGroupName == 'Page_Acfconstructor_Const_Banner' ? ( <Banner item={item} /> ) : null }
-                    { item.fieldGroupName == 'Page_Acfconstructor_Const_Collections' ? ( <Collections item={item} /> ) : null }
-                    { item.fieldGroupName == 'Page_Acfconstructor_Const_Videovoutube' ? ( <Video item={item} /> ) : null }
-                    { item.fieldGroupName == 'Page_Acfconstructor_Const_Instagram' ? ( <Instagram item={item} /> ) : null }
+                    {/*{ item.fieldGroupName == 'Page_Acfconstructor_Const_Banner' ? ( <Banner item={item} /> ) : null }*/}
+                    {/*{ item.fieldGroupName == 'Page_Acfconstructor_Const_Collections' ? ( <Collections item={item} /> ) : null }*/}
+                    {/*{ item.fieldGroupName == 'Page_Acfconstructor_Const_Videovoutube' ? ( <Video item={item} /> ) : null }*/}
+                    {/*{ item.fieldGroupName == 'Page_Acfconstructor_Const_Instagram' ? ( <Instagram item={item} /> ) : null }*/}
                     { item.fieldGroupName == 'Page_Acfconstructor_Const_Slider' ? ( <Slider item={item} /> ) : null }
                     { item.fieldGroupName == 'Page_Acfconstructor_Const_Title' ? ( <Title item={item} /> ) : null }
                     { item.fieldGroupName == 'Page_Acfconstructor_Const_ContentTextImgImg' ? ( <Content1 item={item} /> ) : null }
@@ -32,7 +32,7 @@ const Const = ( { item } ) => {
                     { item.fieldGroupName == 'Page_Acfconstructor_Const_Collaps' ? ( <Collaps item={item} /> ) : null }
                     { item.fieldGroupName == 'Page_Acfconstructor_Const_Content' ? ( <Content3 item={item} /> ) : null }
 
-                    { item.fieldGroupName == 'Page_Acfconstructor_Const_Contactdata' ? ( <Contactdata item={item} /> ) : null }
+                    {/*{ item.fieldGroupName == 'Page_Acfconstructor_Const_Contactdata' ? ( <Contactdata item={item} /> ) : null }*/}
                     {/*{ item.fieldGroupName == 'Page_Acfconstructor_Const_Contactform' ? ( <Contactform item={item} /> ) : null }*/}
 
                 </>
