9d21ebcb447fc62c8550e1515de2fa5d45565d4b
svtom <svtomusjak@gmail.com>
2022-11-14 11:58:02 +0200

    14.11.2022/2

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
+// {useState}
 //import { useFlexSearch } from 'react-use-flexsearch'
 //import { Formik, Form, Field } from 'formik'
 import Layout from '../components/Layout';
 import {graphql, useStaticQuery} from "gatsby";
-import {maxCol} from "../function/SizeCol";
-import styled from "styled-components";
+//import {maxCol} from "../function/SizeCol"; w
+//import styled from "styled-components";
 
 // const index = 0 /* a FlexSearch index */
 // const store = {
@@ -30,7 +31,7 @@ const SearchPage = () => {
     `)
     return (
         <Layout title="Seacrh"  desc={ data.wp.allSettings.generalSettingsTitle } >
-            <Search className="page-404 d-flex justify-content-center align-items-center flex-column">
+            <div className="page-404 d-flex justify-content-center align-items-center flex-column">
                 <h1>SearchPage</h1>
                 {/*<Formik*/}
                 {/*    initialValues={{ query: '' }}*/}
@@ -49,20 +50,20 @@ const SearchPage = () => {
                 {/*        ))}*/}
                 {/*    </ul>*/}
                 {/*</Formik>*/}
-            </Search>
+            </div>
         </Layout>
     );
 };
 export default SearchPage;
 
-const Search = styled.section` 
-    text-align: center;
-    height: calc(100vh - 13.4rem); 
-    @media (max-width: ${maxCol.lg}) {
-         
-    }
-    @media (max-width: ${maxCol.sm}) { 
-        height: calc(100vh - 8.8rem);   
-    } 
-    
-`;
+// const Search = styled.section`
+//     text-align: center;
+//     height: calc(100vh - 13.4rem);
+//     @media (max-width: ${maxCol.lg}) {
+//
+//     }
+//     @media (max-width: ${maxCol.sm}) {
+//         height: calc(100vh - 8.8rem);
+//     }
+//
+// `;
