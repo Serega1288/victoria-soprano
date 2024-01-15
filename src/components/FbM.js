4f15bc45fb212ebf4bf7245a9bd8e4e1cb3a6809
Сергей <svtomusjak@gmail.com>
2024-01-15 11:55:27 +0200

    Update FbM.js

--- src/components/FbM.js
+++ src/components/FbM.js
@@ -2,30 +2,30 @@ import React, { useEffect } from "react";
 
 function MessengerChat() {
     useEffect(() => {
-        window.fbAsyncInit = function () {
-            window.FB.init({
-                xfbml: true,
-                version: "v17.0",
-            });
-        };
-        (function (d, s, id) {
-            var js,
-                fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        })(document, "script", "facebook-jssdk");
+        // window.fbAsyncInit = function () {
+        //     window.FB.init({
+        //         xfbml: true,
+        //         version: "v17.0",
+        //     });
+        // };
+        // (function (d, s, id) {
+        //     var js,
+        //         fjs = d.getElementsByTagName(s)[0];
+        //     if (d.getElementById(id)) return;
+        //     js = d.createElement(s);
+        //     js.id = id;
+        //     js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        //     fjs.parentNode.insertBefore(js, fjs);
+        // })(document, "script", "facebook-jssdk");
     });
     return (
         <>
-            <div id="fb-root" />
-            <div
-                className="fb-customerchat"
-                attribution="page_inbox"
-                page_id="497817423746164"
-            />
+            // <div id="fb-root" />
+            // <div
+            //     className="fb-customerchat"
+            //     attribution="page_inbox"
+            //     page_id="497817423746164"
+            // />
         </>
     );
 }
